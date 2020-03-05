adb171fd7ce6334986886c258f85d820141e2957
tracyjohnson213 <53531005+tracyjohnson213@users.noreply.github.com>
2020-03-04 16:35:38 -0800

    made correction per jshint

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,26 +1,28 @@
 // display drink info in card
 function displayCocktailRecipe(cocktailData){
- // -- data for testing --
-  /*var drinks = {
-    strDrink: "Afterglow",
-    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
-    strInstructions: "Mix. Serve over ice.",
-    strIngredient1: "Grenadine",
-    strIngredient2: "Orange juice",
-    strIngredient3: "Pineapple juice",
-    strIngredient4: null,
-    strMeasure1: "1 part ",
-    strMeasure2: "4 parts ",
-    strMeasure3: "4 parts ",
-    strMeasure4: null
-  }*/
-  // -- end data for testing -- 
-
-  cocktailData = cocktailData.drinks;
-  cocktail = drinks.strDrink;
-  cocktailImage = drinks.strDrinkThumb;
-  cocktailRecipe = drinks.strInstructions;
-  cocktailIngredients = drinks.strMeasure1 + " " + drinks.strIngredient1;
+    // -- data for testing --
+    /*var drinks = {
+      0: {
+        strDrink: "Afterglow",
+        strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+        strInstructions: "Mix. Serve over ice.",
+        strIngredient1: "Grenadine",
+        strIngredient2: "Orange juice",
+        strIngredient3: "Pineapple juice",
+        strIngredient4: null,
+        strMeasure1: "1 part ",
+        strMeasure2: "4 parts ",
+        strMeasure3: "4 parts ",
+        strMeasure4: null
+      }
+    };*/
+    // -- end data for testing -- 
+  
+    cocktailData = drinks;
+    cocktail = cocktailData.strDrink;
+    cocktailImage = cocktailData.strDrinkThumb;
+    cocktailRecipe = cocktailData.strInstructions;
+    cocktailIngredients = cocktailData.strMeasure1 + " " + cocktailData.strIngredient1;
   
     return `
     <div class="container">
@@ -41,14 +43,14 @@ function displayCocktailRecipe(cocktailData){
               </div>
           </div>
       </div>
-    </div>`
+    </div>`;
   }
   
   // get drink data from json
   function fetchCocktailRecipe(event){
     var cocktailName = $("#cocktailName").val();
     if (!cocktailName) {
-      $("#cocktailData").html(`<h2>Please enter a Cocktail name</h2>`)
+      $("#cocktailData").html(`<h2>Please enter a Cocktail name</h2>`);
       return;
     }
     
@@ -58,7 +60,7 @@ function displayCocktailRecipe(cocktailData){
         </div>`);
   
     $.when(
-      $.getJSON(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
+      $.getJSON(url + `/search.php?s=${cocktailName}`)
     ).then(
       function(response) {
         var cocktailData = response;
@@ -74,5 +76,5 @@ function displayCocktailRecipe(cocktailData){
             `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
         }
       }
-    )
+    );
   }
